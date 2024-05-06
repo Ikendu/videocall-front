aae98b64b40ec9d9e332f02f84ddf3fb813033a2
Aniede Chibundu <ndubest56@gmail.com>
2024-05-06 12:06:05 +0100

    change api link

--- src/context/SocketProvider.jsx
+++ src/context/SocketProvider.jsx
@@ -10,7 +10,10 @@ export const useSocket = () => {
 };
 
 function SocketProvider({ children }) {
-  const socket = useMemo(() => io("localhost:8000"), []);
+  const socket = useMemo(
+    () => io(["localhost:8000", "https://videocall-api-1xxh.onrender.com"]),
+    []
+  );
 
   return (
     <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
